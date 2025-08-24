2d935c80b3df367c822a366010d72df5ad73d9a2
Ilya Antonov <dr.creamy.seas@gmail.com>
2025-08-24 20:36:35 +0100

    fix: correct page namings

--- src/clj/pages/gallery/page.clj
+++ src/clj/pages/gallery/page.clj
@@ -5,13 +5,14 @@
    [utils.data.gallery             :as gallery]
    [utils.data.core                :as data]
    [utils.url                      :as url]
+   [utils.config                   :as cfg]
    [pages.gallery.modal            :as modal]
    [pages.gallery.gallery-grid     :as grid]))
 
 (defn render []
   (let [gallery-data (gallery/read-gallery)]
     (layout/main
-     {:title "BigV Gallery"
+     {:title (:title-tag-gallery cfg/config)
       :description "A showcase of fun and action moments"
       :extra-elements [(data/export-data gallery-data "GALLERY_DATA")
                        (data/export-data (dec (count gallery-data)) "GALLERY_DATA_MAX_IDX")]}
