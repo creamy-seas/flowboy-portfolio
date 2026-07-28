3ff47f10bc6172f4c561f4e77b1ec1efb62fdc3b
Ilya Antonov <dr.creamy.seas@gmail.com>
2026-07-28 20:22:34 +0100

    feat: arrows for modal navigation

--- src/clj/pages/gallery/modal.clj
+++ src/clj/pages/gallery/modal.clj
@@ -3,34 +3,52 @@
 
 (def nav-arrow-class
   ;; Very tedious css class to ensure nice clicky navigation arrows
-  "absolute top-1/2 -translate-y-1/2 w-12 h-40
-  flex items-center justify-center
-  bg-fg/20 text-bg/40 ring-mytheme/20 ring-2
-  hover:bg-mytheme hover:text-bg active:bg-mytheme active:text-bg
-  transition-colors duration-200")
+  "flex-1 h-8
+   flex items-center justify-center
+   bg-fg/20 text-bg/60 ring-mytheme/20 ring-2
+   hover:bg-mytheme hover:text-bg
+   active:bg-mytheme active:text-bg
+   transition-colors duration-200
+   rounded-lg")
 
 (defn render
-  "A popup with large display of google content in iframe - to be manipulated using js"
+  "A popup with a large display of Google content in an iframe."
   []
   [:div#gallery-modal.select-none.js-only
    {:class "fixed flex hidden
-            items-center justify-center p-4
-            bg-black/75  inset-0 z-50"}
+            items-center justify-center
+            p-0 sm:p-4
+            bg-black/75 inset-0 z-50"}
+
    [:div.modal-content
-    {:class    "relative bg-bg
-                p-6 rounded-lg
-                w-full lg:max-w-[70vw]
-                overflow-auto"
+    {:class "bg-bg
+             p-3 sm:p-6
+             rounded-lg
+             w-full lg:max-w-[70vw]
+             max-h-screen overflow-auto"
      :onclick "event.stopPropagation()"}
-    [:button#gallery-modal-future.left-4  {:class (str nav-arrow-class " left-2 rounded-tl-xl rounded-bl-xl rounded-tr-sm rounded-br-sm")}
-     [:span.text-3xl "‹"]]
-    [:button#gallery-modal-past.right-4 {:class (str nav-arrow-class " right-2 rounded-tl-xl rounded-bl-xl rounded-tr-sm rounded-br-sm")}
-     [:span.text-3xl "›"]]
-    [:div.w-full.aspect-video
-     [:iframe#gallery-modal-iframe.w-full.h-full
-      {:style           (str "background: url(" (url/put-on-base "/assets/favicon.svg") ") center center no-repeat;")
-       :src             ""
+
+    [:div.w-full.aspect-video.overflow-hidden
+     [:iframe#gallery-modal-iframe.block.w-full.h-full.border-0
+      {:style (str "background: url("
+                   (url/put-on-base "/assets/favicon.svg")
+                   ") center center no-repeat;")
+       :src ""
+       :allow "autoplay; fullscreen; picture-in-picture"
        :allowfullscreen ""
-       :title           ""}]]
+       :title ""}]]
+
+    [:div.flex.gap-4.mt-3
+     [:button#gallery-modal-future
+      {:class nav-arrow-class
+       :aria-label "Show newer gallery item"}
+      [:span.text-3xl "‹"]]
+
+     [:button#gallery-modal-past
+      {:class nav-arrow-class
+       :aria-label "Show older gallery item"}
+      [:span.text-3xl "›"]]]
+
     [:h3#gallery-modal-description.text-xl.font-semibold.text-mytheme.mt-4 ""]
+
     [:p#gallery-modal-date.text-sm.text-gray-400 ""]]])
